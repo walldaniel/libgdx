a41ba731eb93f6b56455bd15d59b3c8890d99090
Daniel_Wall <daniel.wall@ocsbstudent.ca>
2017-09-27 21:00:34 -0400

    initial commit

---
+++ CookieClicker/core/src/states/MenuState.java
@@ -0,0 +1,37 @@
+package states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+
+	public MenuState(GameStateManager gsm) {
+		super(gsm);
+		cam.setToOrtho(false, 600, 400);
+	}
+
+	@Override
+	protected void handleInput() {
+		if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER))
+			gsm.set(new PlayState(gsm));
+	}
+
+	@Override
+	public void update(float dt) {
+		handleInput();
+	}
+
+	@Override
+	public void render(SpriteBatch sb) {
+		sb.begin();
+		sb.end();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
